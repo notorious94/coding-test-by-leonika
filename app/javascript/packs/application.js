5344d1a033e20bb63cf1fd3ab3c95a9455b07be2
notorious94 <akib94dhk@gmail.com>
2022-05-24 20:37:20 +0600

    project setup

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,26 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import { Tooltip, Popover } from "bootstrap"
+import "channels"
+import "bootstrap"
+
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+require("@popperjs/core")
+require("../../assets/stylesheets/application.scss")
+
+$(document).ready(function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+});
